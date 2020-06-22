cf525c378011b784981c3b46b0630b57d9361765
Jessica <duellje@oregonstate.edu>
2020-06-22 17:13:14 -0400

    changed folder system

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import {Link, Switch, Route} from 'react-router-dom'
-import './App.css';
+import './styles/App.css';
 import Client from './components/Client/Client'
 import ClientSignup from './components/Client/ClientSignup'
-import {StyledHeader} from './components/Client/StyledClient'
+import {StyledHeader} from './styles/StyledClient'
 
 function App() {
   return (
