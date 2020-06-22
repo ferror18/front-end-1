38c4eeeac86531e0642170fc1b5abdd0c75bfd99
Jessica <duellje@oregonstate.edu>
2020-06-22 13:12:11 -0400

    set up app to work on client pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom'
 import './App.css';
+import Client from './components/Client/Client'
 
 function App() {
   return (
     <div className="App">
-
+      <Switch>
+        <Route path='/client'>
+          <Client />
+        </Route>
+      </Switch>
     </div>
   );
 }
