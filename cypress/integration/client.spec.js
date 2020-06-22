4a6374546c4fbfa9a35e7a71661fe7fc18d4de89
Jessica <duellje@oregonstate.edu>
2020-06-22 13:20:55 -0400

    set up rotute to client page

--- cypress/integration/client.spec.js
+++ cypress/integration/client.spec.js
@@ -5,4 +5,9 @@ describe('Navigates to page and loads', () => {
         cy.visit('http://localhost:3000')
         cy.url().should('include', 'localhost')
     })
+
+    it('loads page correctly', () => {
+        cy.contains('Anywhere Fitness')
+        cy.contains('Client Login/Signup')
+    })
 })
--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-dom'
+import {Link, Switch, Route} from 'react-router-dom'
 import './App.css';
 import Client from './components/Client/Client'
 
 function App() {
   return (
     <div className="App">
+      <h1> Anywhere Fitness </h1>
       <Switch>
         <Route path='/client'>
           <Client />
+        </Route>
+
+        <Route path='/'>
+        <Link to={'/client'}><button>Client Login/Signup</button></Link>
         </Route>
       </Switch>
     </div>
